4dc7c7145f9d3654e5713dc6298dac5d0cdb1d8d
Santiago Pavon <santiago@dit.upm.es>
2014-04-24 19:56:05 +0200

    Añadido gestion de login y logout.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var postController = require('../controllers/post_controller');
 var userController = require('../controllers/user_controller');
+var sessionController = require('../controllers/session_controller');
 
 
 /* GET home page. */
@@ -10,6 +11,14 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+/* Rutas de sesiones */
+
+router.get('/login',  sessionController.new); 
+                     // obtener el formulario a rellenar para hacer login. 
+router.post('/login', sessionController.create); 
+                     // enviar formulario para crear la sesión.
+router.get('/logout', sessionController.destroy); 
+                     // destruir la sesión actual.
 
 /* Autoloading */
 
@@ -19,12 +28,17 @@ router.param('userid', userController.load); // autoload :userid
 /* Rutas de Posts */
 
 router.get('/posts', postController.index);
-router.get('/posts/new', postController.new);
+router.get('/posts/new', sessionController.loginRequired,
+                         postController.new);
 router.get('/posts/:postid([0-9]+)', postController.show);
-router.post('/posts', postController.create);
-router.get('/posts/:postid([0-9]+)/edit', postController.edit);
-router.put('/posts/:postid([0-9]+)', postController.update);
-router.delete('/posts/:postid([0-9]+)', postController.destroy);
+router.post('/posts', sessionController.loginRequired,
+                      postController.create);
+router.get('/posts/:postid([0-9]+)/edit', sessionController.loginRequired,
+                                          postController.edit);
+router.put('/posts/:postid([0-9]+)', sessionController.loginRequired,
+                                     postController.update);
+router.delete('/posts/:postid([0-9]+)', sessionController.loginRequired,
+                                        postController.destroy);
 
 /* Rutas de Users */
 
@@ -32,9 +46,12 @@ router.get('/users', userController.index);
 router.get('/users/new', userController.new);
 router.get('/users/:userid([0-9]+)', userController.show);
 router.post('/users', userController.create);
-router.get('/users/:userid([0-9]+)/edit', userController.edit);
-router.put('/users/:userid([0-9]+)', userController.update);
-router.delete('/users/:userid([0-9]+)', userController.destroy);
+router.get('/users/:userid([0-9]+)/edit', sessionController.loginRequired,
+                                          userController.edit);
+router.put('/users/:userid([0-9]+)', sessionController.loginRequired,
+                                     userController.update);
+router.delete('/users/:userid([0-9]+)', sessionController.loginRequired,
+                                        userController.destroy);
 
 
 
